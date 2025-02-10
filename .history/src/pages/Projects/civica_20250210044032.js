5e2315bc96829fafa691204bda6787c7a7bb5f59
madelinestruthers <mstruthers1@my.bcit.ca>
2025-02-10 04:43:16 -0800

    eferf

--- .history/src/pages/Projects/civica_20250210044032.js
+++ .history/src/pages/Projects/civica_20250210044032.js
@@ -96,7 +96,7 @@ export default function Civica() {
           {/* Wireframes & Prototypes */}
           <section className={styles.prototypeSection}>
             <h3>Figma Hifi Wireframes</h3>
-            <iframe width="900" height="750" src="https://embed.figma.com/design/0NxjFtht1BziwLShVjdx5B/Civica-2.0?node-id=0-1&embed-host=share" allowFullScreen></iframe>
+            <iframe width="800" height="450" src="https://embed.figma.com/design/0NxjFtht1BziwLShVjdx5B/Civica-2.0?node-id=0-1&embed-host=share" allowFullScreen></iframe>
 
           </section>
 
@@ -138,7 +138,7 @@ In response, new logo concepts were developed to better align with the brand’s
           <section>
           <h3>Figma Interactive Prototype</h3>
           <br/><br/>
-            <iframe width="900" height="600" src="https://embed.figma.com/proto/0NxjFtht1BziwLShVjdx5B/Civica-2.0?node-id=30-1676&starting-point-node-id=30%3A1600&embed-host=share" allowFullScreen></iframe>
+            <iframe width="800" height="450" src="https://embed.figma.com/proto/0NxjFtht1BziwLShVjdx5B/Civica-2.0?node-id=30-1676&starting-point-node-id=30%3A1600&embed-host=share" allowFullScreen></iframe>
           </section>
         </main>
       </div>
